329e19738cdc6d0979ce39cfda4780415bc4c65d
Anderson-Andre-P <eleu.ander@gmail.com>
2025-09-24 18:47:04 -0300

    ffeat: realizando operações

--- aula_004/principal.cpp
+++ aula_004/principal.cpp
@@ -16,7 +16,25 @@ int main()
   cout << "n2 = ";
   n2.print();
 
+  cout << endl
+       << endl;
+
+  Complexo n3;
+  Complexo n4;
+
+  n3 = n1.add(n2); // n3 = n1 + n2
+  n4 = n1.sub(n2); // n4 = n1 - n2
+
+  cout << "n3 (add) = ";
+  n3.print();
+
   cout << endl;
+
+  cout << "n4 (sub) = ";
+  n4.print();
+
+  cout << endl
+       << endl;
 
   return 0;
 }
