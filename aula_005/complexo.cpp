5698e8b69b14fc855bb290c3ec6c3d89f9f6aa90
Anderson-Andre-P <eleu.ander@gmail.com>
2025-09-27 13:00:15 -0300

    feat: forma comum de criar funções com sobrecarga de operadores

---
+++ aula_005/complexo.cpp
@@ -0,0 +1,72 @@
+#ifndef COMPLEXO_H
+#define COMPLEXO_H
+
+#include <iostream>
+
+using namespace std;
+
+class Complexo
+{
+private:
+  double re, im;
+
+public:
+  Complexo(double = 0, double = 0);
+  ~Complexo() {}
+  double get_real() { return re; }
+  double get_imag() { return im; }
+
+  // Definindo os operadores
+  Complexo operator+(Complexo &); // +
+  Complexo operator-(Complexo &); // -
+
+  void print();
+};
+
+#endif
+
+// Construtor
+Complexo::Complexo(double r, double i) : re{r}, im{i} {}
+
+// Sobrecarga de operadores
+// Soma
+Complexo Complexo::operator+(Complexo &c)
+{
+  double r, i;
+  r = re + c.get_real();
+  i = im + c.get_imag();
+  return Complexo{r, i};
+}
+
+// Subtração
+Complexo Complexo::operator-(Complexo &c)
+{
+  double r, i;
+  r = re - c.get_real();
+  i = im - c.get_imag();
+  return Complexo(r, i);
+}
+
+void Complexo::print()
+{
+  cout << re << " + (" << im << ")i" << endl
+       << endl;
+}
+
+int main()
+{
+  Complexo a{10, 0};
+  Complexo b(10, 15);
+
+  a = a + b; // notação direta e simples
+  // a = a.operator+(b); // notação explícita funcional
+  b = b - a; // como em representação aritmética comum
+
+  cout << "Números criados: " << endl;
+
+  a.print();
+
+  b.print();
+
+  return 0;
+}
