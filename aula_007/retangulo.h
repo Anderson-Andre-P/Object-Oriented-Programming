99e6f3b8fd4f47557808fda512ec5d49c435cc6e
Anderson-Andre-P <eleu.ander@gmail.com>
2025-09-30 21:44:55 -0300

    feat: sobrescreve as funções virtuais para o polimorfismo

--- aula_007/retangulo.h
+++ aula_007/retangulo.h
@@ -6,7 +6,16 @@
 class Retangulo : public Forma
 {
 private:
+  double b, h;
+
 public:
+  Retangulo(double, double, double, double); // x, y, b, h
+
+  ~Retangulo() {};
+
+  void imprime_dados();
+
+  double area();
 }
 
 #endif
