8f9751568215f1b8a0b586883a4f3d182de03a1b
Anderson-Andre-P <eleu.ander@gmail.com>
2025-09-30 22:10:43 -0300

    feat: cria um vetor de formas

--- aula_007/main.cpp
+++ aula_007/main.cpp
@@ -7,29 +7,44 @@ using namespace std;
 
 int main()
 {
-  cout << "Forma: " << endl;
+  // cout << "Forma: " << endl;
 
-  Forma *ponto1 = new Forma(10.0, 20.0);
+  // Forma *ponto1 = new Forma(10.0, 20.0);
 
-  ponto1->imprime_dados();
+  // ponto1->imprime_dados();
 
-  cout << "Area = " << ponto1->area() << endl;
+  // cout << "Area = " << ponto1->area() << endl;
 
-  cout << "\n";
+  // cout << "\n";
 
-  cout << "Retangulo: " << endl;
+  // cout << "Retangulo: " << endl;
 
-  Retangulo *ret1 = new Retangulo(0, 0, 10, 50);
+  // Retangulo *ret1 = new Retangulo(0, 0, 10, 50);
 
-  ret1->imprime_dados();
+  // ret1->imprime_dados();
 
-  cout << "\n";
+  // cout << "\n";
 
-  cout << "Circulo: " << endl;
+  // cout << "Circulo: " << endl;
 
-  Circulo *circ1 = new Circulo(0, 0, 10);
+  // Circulo *circ1 = new Circulo(0, 0, 10);
 
-  circ1->imprime_dados();
+  // circ1->imprime_dados();
+
+  Forma *vector_formas[5];
+
+  vector_formas[0] = new Retangulo{10, 30, 45, 21};
+  vector_formas[1] = new Forma{100, 120};
+  vector_formas[2] = new Circulo{0, 0, 15.5};
+  vector_formas[3] = new Circulo{9, 0.75, 18};
+  vector_formas[4] = new Retangulo{0, 0, 10, 10};
+
+  for (auto x : vector_formas)
+  {
+    cout << "Forma: " << endl;
+    x->imprime_dados();
+    cout << "\n\n";
+  }
 
   return 0;
 }
