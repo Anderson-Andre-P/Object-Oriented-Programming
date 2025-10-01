805c6750e65f0ab365593911830e84939bd0a969
Anderson-Andre-P <eleu.ander@gmail.com>
2025-09-30 21:41:46 -0300

    feat: cria as funções para executar o algoritmo

--- aula_007/main.cpp
+++ aula_007/main.cpp
@@ -1,8 +1,15 @@
+#include "forma.h"
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
+  Forma ponto1(10.0, 20.0);
+
+  ponto1.imprime_dados();
+
+  cout << "Area = " << ponto1.area() << endl;
+
   return 0;
 }
